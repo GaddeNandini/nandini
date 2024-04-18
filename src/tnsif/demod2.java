24f8bf8d57a7ea3aaf24ec2994293a4911081875
USER <USER@192.168.1.6>
2024-04-19 02:05:52 +0530

    commit2

---
+++ src/tnsif/demod2.java
@@ -0,0 +1,21 @@
+package tnsif;
+
+public class demod2 {
+	int a=10;
+	static String m="Nandini";
+	public static void Test() {
+		System.out.println("hi"+"I am"+m);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		demod2 d=new demod2();
+		demod2.Test();
+		System.out.println(d);
+		
+		
+		
+
+	}
+
+}
